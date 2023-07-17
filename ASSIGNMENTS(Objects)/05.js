3196e8263c73f42cdc75d9977790c7d8ea2873f5
BatsyKun <119741750+KoochieKun@users.noreply.github.com>
2023-07-18 00:36:44 +0530

    complete assignments

--- ASSIGNMENTS(Objects)/05.js
+++ ASSIGNMENTS(Objects)/05.js
@@ -1,11 +1,12 @@
-let input1 = "den"
-let input2 = "end"
+
+let input1 = "happy"
+let input2 = "happy"
 
 function checkAnagrams(w0ord1, w0ord2){
     let word1 = w0ord1.split("")
     let word2 = w0ord2.split("")
 
-    if(word1.length !== word2.length) return "False"
+    if(word1.length !== word2.length) return False
 
     let obj1 = word1.reduce((acc, curValue) => {
         acc[curValue] = (acc[curValue] || 0) + 1;
@@ -16,9 +17,11 @@ function checkAnagrams(w0ord1, w0ord2){
         return acc
     }, {})
 
-    if(obj1===obj2) return true
-
+    for (let key1 in obj1) {
+        if(!word2.includes(key1)) return false
+        if(!obj1[key1]== obj2[key1]) return false
+        else return true
+    }       
 }
-
 
 console.log(checkAnagrams(input1,input2))
